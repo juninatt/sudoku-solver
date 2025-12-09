f04e8e42d4fc4cf9169726f007ed5ad82da7af97
Petter Bergström <petter.bt@proton.me>
2025-12-09 16:13:29 +0100

    chore(app): remove unused constant from AppConstants

--- app/src/main/java/se/pbt/sudokusolver/app/AppConstants.java
+++ app/src/main/java/se/pbt/sudokusolver/app/AppConstants.java
@@ -2,8 +2,6 @@ package se.pbt.sudokusolver.app;
 
 public class AppConstants {
 
-    public static final String MAIN_MENU_VIEW = "main-menu-view.fxml";
-
     public static final String I18N_TITLE_MAIN = "ui.title.main";
 
 }
